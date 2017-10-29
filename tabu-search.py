e947ab53865971693d0b5ed067f77f964ce10be8
Raul Lozano Martin <raullm7@gmail.com>
2017-10-29 22:06:20 +0000

    Improved tabu search

--- tabu-search.py
+++ tabu-search.py
@@ -15,79 +15,87 @@ def length(point1, point2):
     return math.sqrt((point1.x - point2.x)**2 + (point1.y - point2.y)**2)
 
 
-def greedy():
-    sol = [0] * (len(distances))
-    total_distance = 0
-    for city_index in range(len(distances)):
-        distances_from_city = [0] * len(distances[city_index])
-        for i in range(len(distances[city_index])):
-            distances_from_city[i] = (i, distances[city_index][i])
-
-        distances_from_city.sort(key = lambda x: x[1])
-        for next_city_distance in distances_from_city:
-            if next_city_distance[1] == 0:
-                continue
-            next_city_index = next_city_distance[0]
-            if next_city_index not in sol:
-                total_distance += next_city_distance[1]
-                sol[city_index] = next_city_index
-                break
-    return sol, total_distance
+def random_solution():
+    perm = np.arange(node_count)
+    np.random.shuffle(perm)
+    return perm
 
 
-def swap_cities(city1, city2, solution):
-    this_solution = copy.deepcopy(solution)
-    temp = this_solution[city1]
-    this_solution[city1] = this_solution[city2]
-    this_solution[city2] = temp
-    return this_solution
+def generate_new_solution(solution):
+    new_solution = copy.deepcopy(solution)
+    first_city, second_city = np.random.randint(new_solution.size), np.random.randint(new_solution.size)
+
+    exclude = [copy.copy(first_city)]
+    exclude.append((len(new_solution) if first_city == 0 else first_city - 1))
+    exclude.append((0 if first_city == new_solution.size - 1 else first_city + 1))
+
+    while second_city in exclude:
+        second_city = np.random.randint(len(new_solution))
+
+    if second_city < first_city:
+        first_city, second_city = second_city, first_city
+
+    new_solution[first_city:second_city] = np.flip(new_solution[first_city:second_city], 0)
+    return new_solution, [[solution[first_city - 1], solution[first_city]], [solution[second_city - 1], solution[second_city]]]
 
 
-def decrement_tabu(tabu_list):
-    for i in range(len(tabu_list)):
-        for j in range(len(tabu_list)):
-            tabu_list[i][j] -= (0 if tabu_list[i][j] <= 0 else 1)
-    return tabu_list
+def is_tabu(permutation, tabu_list):
+    for i in range(len(permutation)):
+        second_city = permutation[0] if i == (len(permutation) - 1) else permutation[i + 1]
+        for tabu in tabu_list:
+            return tabu == [permutation[i], second_city]
+    return False
 
 
-def tabu_move(tabu_list, city1, city2):
-    tabu_list[city1][city2] += 10
-    tabu_list[city2][city1] += 10
-    return  tabu_list
+def generate_candidate(current_solution, tabu_list):
+    new_solution, edges = [], None
+    old_cost = 0
+    new_cost = 1
+
+    while not len(new_solution) or (is_tabu(new_solution, tabu_list)):
+        old_cost = new_cost
+        new_solution, edges = generate_new_solution(current_solution)
+        new_cost = get_cost(new_solution)
+        if old_cost * 0.9 > new_cost:
+            break
+
+    candidate_cost = new_cost
+    candidate = copy.copy(new_solution)
+    return [candidate, edges, candidate_cost]
 
 
-def get_best_neighbour(tabu_list, init_solution):
-    best_solution = copy.deepcopy(init_solution)
-    best_cost = get_cost(init_solution)
-    city1 = 0
-    city2 = 0
-    first_neighbour = True
-    current_cost = best_cost
-    for i in range(len(best_solution)):
-        for j in range(len(best_solution)):
-            if i == j:
-                continue
+def search(tabu_list_size, candidate_list_size, max_iter):
+    current_solution = random_solution()
+    current_cost = get_cost(current_solution)
+    best_solution = copy.deepcopy(current_solution)
+    best_solution_cost = current_cost
 
-            cost_before_swap = current_cost
-            current_solution = swap_cities(i, j, init_solution)
-            current_cost = get_cost(current_solution)
+    tabu_list = [0] * tabu_list_size
+    candidates = [0] * candidate_list_size
 
-            if current_cost < best_cost * 0.8:
-                # If current solution gives a result a 20% better, relax the tabu constraint
-                tabu_list[i][j] = 0
+    for iteration in range(max_iter):
+        for i in range(candidate_list_size):
+            candidates[i] = generate_candidate(current_solution, tabu_list)
 
-            if (current_cost < best_cost or first_neighbour) and tabu_list[i][j] == 0:
-                first_neighbour = False
-                city1 = i
-                city2 = j
-                best_solution = copy.deepcopy(current_solution)
-                best_cost = current_cost
+        candidates.sort(key=lambda x: x[2])
+        first_candidate = candidates[0]
 
-    if city1 != 0:
-        tabu_list = decrement_tabu(tabu_list)
-        tabu_list = tabu_move(tabu_list, city1, city2)
+        best_candidate_solution = first_candidate[0]
+        best_candidate_edges = first_candidate[1]
+        best_candidate_cost = first_candidate[2]
 
-    return best_solution, tabu_list
+        if best_candidate_cost < current_cost:
+            current_solution = best_candidate_solution
+            if best_candidate_cost < best_solution_cost:
+                best_solution = best_candidate_solution
+                best_solution_cost = best_candidate_cost
+
+            for edge in best_candidate_edges:
+                np.insert(tabu_list, 0, edge)
+
+            while len(tabu_list) > tabu_list_size:
+                tabu_list = np.delete(tabu_list, 0)
+    return best_solution
 
 
 def get_cost(solution):
@@ -97,19 +105,9 @@ def get_cost(solution):
     return cost
 
 
-def get_cost_to_print(solution):
-    cost = distances[0, solution[0]]
-    for i in range(len(solution) - 1):
-        cost += distances[solution[i], solution[i + 1]]
-    cost += distances[solution[len(solution) - 1], 0]
-    return cost
-
-
 def solve_it(input_data):
     global distances
     global node_count
-    # Modify this code to run your optimization algorithm
-
     # parse the input
     lines = input_data.split('\n')
 
@@ -129,23 +127,11 @@ def solve_it(input_data):
                 distances[i][j] = d
                 distances[j][i] = d
 
-    (current_solution, cost) = greedy()
-
-    number_iterations = 1000
-    tabu_list = np.zeros((node_count, node_count))
-
-    best_solution = copy.deepcopy(current_solution)
+    max_iter = 1000
+    tabu_list_size = round(node_count / 4)
+    max_candidates = node_count * 3
+    best_solution = search(tabu_list_size, max_candidates, max_iter)
     best_cost = get_cost(best_solution)
-
-    for i in range(number_iterations):
-        current_solution, tabu_list = get_best_neighbour(tabu_list, current_solution)
-        current_cost = get_cost(current_solution)
-
-        if current_cost < best_cost:
-            print('Current best cost: ' + str(current_cost))
-            best_solution = copy.deepcopy(current_solution)
-            best_cost = current_cost
-
 
     # prepare the solution in the specified output format
     output_data = '%.2f' % best_cost + ' ' + str(0) + '\n'
